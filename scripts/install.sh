1d918e1e89fc99d911cb9fad9c057ed51ec0d77f
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-02-04 09:42:41 -0700

    Kernel panic during Docker builds. Resolved by adding a yum update to the installation script.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
 echo 'other_args="--bip 10.1.1.1/24 -H 0.0.0.0:2376 -G docker"' >> /etc/sysconfig/docker
+yum -y update
 yum -y install device-mapper-event-libs
 yum -y install docker-io
 service docker start
