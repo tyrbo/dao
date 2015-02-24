889f18d0c6eec95e78853ba5ab26391d07b4c258
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-02-23 19:51:51 -0700

    Removed old Vagrant files. Adjusted the install script. Still needs boot2docker additions and the use of our custom ISO. DOCKER_HOST readme needs to be updated to use the new workflow.

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-echo 'other_args="--bip 10.1.1.1/24 -H 0.0.0.0:2376 -G docker"' >> /etc/sysconfig/docker
-yum -y install perl dkms
-/etc/init.d/vboxadd setup
-yum -y update
-yum -y install device-mapper-event-libs
-yum -y install docker-io
-service docker start
-chkconfig docker on
