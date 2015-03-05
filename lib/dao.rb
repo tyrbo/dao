0ecb7c5d75d0ac088bcf8550ae598000c430e0ca
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-03-04 23:30:43 -0700

    Adjusts Dao::Base to be a little more fault tolerant, in the event that Docker isn't running for some reason. Adds a test for the Dao::Forwarder class

--- lib/dao.rb
+++ lib/dao.rb
@@ -6,36 +6,40 @@ require 'containers'
 
 module Dao
   class Base
-    attr_reader :ip, :containers, :servers
+    attr_reader :containers, :servers
 
     def initialize
       @containers = Dao::Containers.new
-      @ip = `boot2docker ip`.strip # gross.
       @servers = []
     end
-    
+
     def run
       Dao::DNSServer.new(5300).start
 
       loop do
-        old_ports = servers.map(&:port)
-        mappings = Dao::PortMapper.create(ip, containers.matching)
+        begin
+          ip = `boot2docker ip`.strip # gross.
+          old_ports = servers.map(&:port)
+          mappings = Dao::PortMapper.create(ip, containers.matching)
 
-        mappings.each do |port, _|
-          if servers.none? { |x| x.port == port }
-            @servers << Dao::HTTPServer.new(port).start
+          mappings.each do |port, _|
+            if servers.none? { |x| x.port == port }
+              @servers << Dao::HTTPServer.new(port).start
+            end
           end
+
+          new_ports = mappings.map { |port, _| port }
+          stale_ports = old_ports - new_ports
+
+          stale_ports.each do |port|
+            idx = servers.find_index { |s| s.port == port }
+            servers.delete_at(idx).stop
+          end
+
+          sleep 15
+        rescue
+          sleep 15
         end
-
-        new_ports = mappings.map { |port, _| port }
-        stale_ports = old_ports - new_ports
-
-        stale_ports.each do |port|
-          idx = servers.find_index { |s| s.port == port }
-          servers.delete_at(idx).stop
-        end
-
-        sleep 15
       end
     end
   end
--- spec/forwarder_spec.rb
+++ spec/forwarder_spec.rb
@@ -1,4 +1,12 @@
 require 'spec_helper'
 
 describe Dao::Forwarder do
+  describe '#rewrite_env' do
+    it 'returns the proper address for a given host' do
+      hash = { 'HTTP_HOST' => '192.168.59.103:1337' }
+
+      allow(Dao::PortMapper).to receive(:get_addr_for).and_return('192.168.59.103:1337')
+      expect(Dao::Forwarder.new.rewrite_env({})).to eq hash
+    end
+  end
 end
