f94ab2052118275aa9af3c44bfa0c6d2d1dd1878
Jonmichael Chambers <jonmichael.chambers@livingsocial.com>
2015-04-24 12:16:50 -0600

    Swaps from class << self to using self. for methods

--- lib/port_mapper.rb
+++ lib/port_mapper.rb
@@ -1,43 +1,41 @@
 module Dao
   class PortMapper
-    class << self
-      def create(ip, containers)
-        @mappings = {}
-        @ip = ip
+    def self.create(ip, containers)
+      @mappings = {}
+      @ip = ip
 
-        containers.each do |container|
-          info = container.info
-          ports = get_ports_for(info)
-          name = get_name_for(info)
-          
-          ports.each do |port|
-            public_port = port['PublicPort']
-            private_port = port['PrivatePort']
+      containers.each do |container|
+        info = container.info
+        ports = get_ports_for(info)
+        name = get_name_for(info)
 
-            @mappings[private_port] ||= []
-            @mappings[private_port] << { name: name, public_port: public_port, ip: @ip }
-          end
+        ports.each do |port|
+          public_port = port['PublicPort']
+          private_port = port['PrivatePort']
+
+          @mappings[private_port] ||= []
+          @mappings[private_port] << { name: name, public_port: public_port, ip: @ip }
         end
-
-        @mappings
       end
 
-      def get_name_for(info)
-        name = info['Names'].first
-        /^\/([A-z0-9]+?)_.*?_\d+/.match(name)[1]
-      end
+      @mappings
+    end
 
-      def get_ports_for(info)
-        ports = info['Ports'].select { |p| p['PublicPort'] }
-      end
+    def self.get_name_for(info)
+      name = info['Names'].first
+      /^\/([A-z0-9]+?)_.*?_\d+/.match(name)[1]
+    end
 
-      def get_addr_for(req)
-        name = req.host.split('.')[0]
-        port = req.port
-        priv_port = @mappings[port].detect { |x| x[:name] == name }[:public_port]
+    def self.get_ports_for(info)
+      ports = info['Ports'].select { |p| p['PublicPort'] }
+    end
 
-        "#{@ip}:#{priv_port}"
-      end
+    def self.get_addr_for(req)
+      name = req.host.split('.')[0]
+      port = req.port
+      priv_port = @mappings[port].detect { |x| x[:name] == name }[:public_port]
+
+      "#{@ip}:#{priv_port}"
     end
   end
 end
